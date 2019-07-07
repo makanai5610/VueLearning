47c4650a8f1753c198f0ccbe6c53dae94f716339
Takumi Karibe <takumi.k.5610@gmail.com>
2019-07-07 17:45:15 +0900

    fix

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,14 +2,18 @@ use actix_web::{App, HttpServer};
 use actix_files;
 
 const PORT: &str = "localhost:3000";
+const FILE: &str = "index.html";
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
-    let path = format!("../{}/{}", args.get(1).unwrap(), args.get(2).unwrap());
+    let (chapter_number, project_name) = (args.get(1).unwrap(), args.get(2).unwrap());
+    let path = format!("../{}/{}", chapter_number, project_name);
+
+    println!("serve {}/{}", path, FILE);
 
     HttpServer::new(move || {
         App::new()
-            .service(actix_files::Files::new("/", path.clone()).index_file("index.html"))
+            .service(actix_files::Files::new("/", path.clone()).index_file(FILE))
     })
     .bind(PORT)?
     .run()
