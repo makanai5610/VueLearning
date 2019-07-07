9be356b4aed7800cfa71a333f74928ea48dc46f4
Takumi Karibe <takumi.k.5610@gmail.com>
2019-07-07 19:00:01 +0900

    props sample

---
+++ 03/props/props.js
@@ -0,0 +1,13 @@
+Vue.component('item-desc', {
+    props: {
+        itemName: {
+            type: String
+        }
+    },
+    template: '<p>{{ itemName }}は便利です。</p>'
+})
+
+new Vue({
+    el: '#app',
+    data: { myItem: 'pen' }
+})
