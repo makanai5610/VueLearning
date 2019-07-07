de34414e8e007b04b6c21f7dd044aa43f05c075d
Takumi Karibe <takumi.k.5610@gmail.com>
2019-07-07 16:37:12 +0900

    諦め

--- 02/sample/src/main.rs
+++ 02/sample/src/main.rs
@@ -3,23 +3,10 @@ use actix_files;
 
 const PORT: &str = "localhost:3000";
 
-enum Path {
-    Items, Lifecycle
-}
-
-impl Path {
-    fn build(&self) -> String {
-        format!("../static/{}", match self {
-            Path::Items => "items",
-            Path::Lifecycle => "lifecycle"
-        })
-    }
-}
-
 fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(actix_files::Files::new("/", Path::Items.build()).index_file("index.html"))
+            .service(actix_files::Files::new("/", "../static/items").index_file("index.html"))
     })
     .bind(PORT)?
     .run()
