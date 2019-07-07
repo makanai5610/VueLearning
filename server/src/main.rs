07c0d1e796400464a6472fd64b612cba6e5824c6
Takumi Karibe <takumi.k.5610@gmail.com>
2019-07-07 17:12:14 +0900

    優勝

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,9 +4,12 @@ use actix_files;
 const PORT: &str = "localhost:3000";
 
 fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let args: Vec<String> = std::env::args().collect();
+    let path = format!("../{}/{}", args.get(1).unwrap(), args.get(2).unwrap());
+
+    HttpServer::new(move || {
         App::new()
-            .service(actix_files::Files::new("/", "../static/items").index_file("index.html"))
+            .service(actix_files::Files::new("/", path.clone()).index_file("index.html"))
     })
     .bind(PORT)?
     .run()
